9165a221716b50eb8061067934db49be9b7f45ab
mjuniper <mjuniper@gmail.com>
2016-03-25 11:07:47 -0600

    better deploy

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,17 @@
 
-# dev
-aws s3 cp post-iframe.html s3://dev-od-umbrella --acl public-read
+ENV="$1"
 
-# qa
-aws s3 cp post-iframe.html s3://open-data-qa-umbrella-tng --acl public-read
+echo "deploying to: $ENV"
 
-# prod
-# aws s3 cp post-iframe.html s3://open-data-prod-umbrella-tng --acl public-read --profile=prod
+if [ "$ENV" = "devext" ] ; then
+  # dev
+  aws s3 cp post-iframe.html s3://dev-od-umbrella --acl public-read
+elif [ "$ENV" = "qaext" ] ; then
+  # qa
+  aws s3 cp post-iframe.html s3://open-data-qa-umbrella-tng --acl public-read
+elif [ "$ENV" = "prod" ] ; then
+  # prod
+  aws s3 cp post-iframe.html s3://open-data-prod-umbrella-tng --acl public-read --profile=prod
+else
+  echo "Environment not found!"
+fi
